40183d7b0db9e182e18bef02ec0d7453ac59257c
izoom1 <emmarivas04@gmail.com>
2020-02-21 01:13:04 +0000

    added weather bot

--- nimbot.py
+++ nimbot.py
@@ -14,3 +14,11 @@ async def play_nim(ctx):
 
 bot.run(token)
 
+import pandas as pd
+@bot.command(name='weatherman')
+async def weather(ctx):
+url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?aggregateHours=24&shortColumnNames=false&sendAsDatasource=false&allowAsynch=false&contentType=csv&unitGroup=us&key=DDBCK6MYKAUWQ5K6SYJ7APGYL&locations=north%20bergen%2C%20new%20jersey"
+weather_data = pd.read_csv(url)
+    await ctx.send(weather_data.pivot_table(values=["Minimum Temperature", "Maximum Temperature", "Chance Precipitation (%)", "Conditions"], index="Date time", fill_value=0))
+
+bot.run(token)
